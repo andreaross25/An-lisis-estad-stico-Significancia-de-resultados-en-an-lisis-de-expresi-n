f7f58f93d982138fdebe973aed13461c085f3a4a
Andrea Ross <111784635+andreaross25@users.noreply.github.com>
2023-02-07 21:19:08 -0700

    Update estadistico_expresion.py
    Se agrega el análisis de correlación Spearman y Pearson

--- estadistico_expresion.py
+++ estadistico_expresion.py
@@ -1,5 +1,5 @@
 #Significancia_estadistica_analisis_de_expresion
-#Version 1
+#Version 2
 #Andrea_Ross
 
 import numpy as np
@@ -12,6 +12,7 @@ from scipy.stats import shapiro
 def test_estadistico(df1, df2, columna):
     p1 = shapiro(df1[columna])[1]
     p2 = shapiro(df2[columna])[1]
+    es_normal = False
     if p1 > 0.05 and p2 > 0.05:
         t_stat, p_value = stats.ttest_ind(df1[columna], df2[columna])
         print(f'Prueba t de Student para {columna}:')
@@ -19,6 +20,7 @@ def test_estadistico(df1, df2, columna):
         print('p-value:', p_value)
         if p_value <= 0.05:
             print("Este valor es estadísticamente significativo")
+        es_normal = True
         print('\n')
     else:
         u_stat, p_value = stats.mannwhitneyu(df1[columna], df2[columna])
@@ -28,6 +30,7 @@ def test_estadistico(df1, df2, columna):
         if p_value <= 0.05:
             print("Este valor es estadísticamente significativo")
         print('\n')
+    return es_normal
 
 # Imprimir en la terminal los valores de significancia
 df = pd.read_csv('/Users/andreaross/Desktop/EPCM.csv')
@@ -40,3 +43,32 @@ test_estadistico(df_experiment, df_control, 'miR141')
 test_estadistico(df_experiment, df_control, 'miR145')
 test_estadistico(df_experiment, df_control, 'miR146')
 test_estadistico(df_experiment, df_control, 'miR148')
+
+
+#Correlación entre los genes o miRNAS de estudio, Pearson para paramétricos y Spearman para no paramétricos
+def analisis_correlacion(df, mirnas, test_normalidad):
+    mirnas: ['miR141', 'miR145', 'miR146', 'miR148']
+    test_normalidad: [test_estadistico(df_experiment, df_control, 'miR141'),
+                test_estadistico(df_experiment, df_control, 'miR145'),
+                test_estadistico(df_experiment, df_control, 'miR146'),
+                test_estadistico(df_experiment, df_control, 'miR148')]
+    
+    corr_method = 'pearson' if all(test_normalidad) else 'spearman'
+    corr_matrix = df[mirnas].corr(method=corr_method)
+    print(f'Análisis de correlación ({corr_method}):')
+    print(corr_matrix)
+    print('\n')
+    
+    significant_corrs = corr_matrix[(corr_matrix <= 0.05) & (corr_matrix >= 0)].stack().reset_index()
+    reported_pairs = []
+    for row in significant_corrs.itertuples(index=False):
+        miRNA1, miRNA2 = sorted([row.level_0, row.level_1])
+        if (miRNA1, miRNA2) not in reported_pairs:
+            print(f"{row[0]} and {row[1]} with a correlation of {row[2]:.3f}")
+            reported_pairs.append((miRNA1, miRNA2))
+            print('\n')
+
+# Imprimir la matriz de correlación
+test_normalidad = [True, True, True, True]
+analisis_correlacion(df, ['miR141', 'miR145', 'miR146', 'miR148'], test_normalidad)
+
